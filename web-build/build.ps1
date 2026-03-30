1901d30028444458034ddad4716b457ef5eefc6d
Ethan Silvio <271342194+Crox209@users.noreply.github.com>
2026-03-30 22:40:53 +0000

    Beta2

--- web-build/build.ps1
+++
@@ -1,4 +0,0 @@
-# Run the Emscripten web build from the project root
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location (Join-Path $scriptDir "project\emscripten")
-& (Join-Path $scriptDir "project\emscripten\build_web.ps1")
